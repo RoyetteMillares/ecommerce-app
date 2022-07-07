97d25b06851b0757e84aafbcf43492f0d68674f5
Royette Millares <royettemillares.ifl@gmail.com>
2022-07-07 15:37:51 +0800

    dynamic content done

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
+import { urlFor } from '../lib/client'
 
 const HeroBanner = ({heroBanner}) => {
   return (
@@ -9,15 +10,15 @@ const HeroBanner = ({heroBanner}) => {
       <p className="beats-solo">{heroBanner.smallText}</p>
       <h3>{heroBanner.midText}</h3>
       <h1>{heroBanner.largeText1}</h1>
-      <img src={''} alt='headphones' className='hero-banner-image'/>
+      <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image'/>
 
       <div>
-        <Link href='/product/ID'>
-          <button type='button'>BUTTON TEXT</button>
+        <Link href={`/product/${heroBanner.product}`}>
+          <button type='button'>{heroBanner.buttonText}</button>
         </Link>
         <div className='desc'>
-          <h5>Desc</h5>
-          <p>DESCZXCX</p>
+          <h5>Description</h5>
+          <p>{heroBanner.desc}</p>
         </div>
       </div>
 
