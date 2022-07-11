0d3e8f7c972fbf57a168a54094ae597f9b2576b9
Royette Millares <royettemillares.ifl@gmail.com>
2022-07-12 07:30:03 +0800

    continue later

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,39 @@
 import React from 'react'
 
-import {client} from '../lib/client';
-import {Product, FooterBanner, HeroBanner} from '../components'
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components'
 
 
-const Home = ({ productsData, bannerData}) => (
+const Home = ({ productsData, bannerData }) => (
 
-    <div>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
-      
-      <div className='products-heading'>
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
-      <div className='products-container'>
-        {productsData.map(
-          (products) => <Product key={products._id} product={products}/>
-        )}
-      </div>
-      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+
+    <div className='products-heading'>
+      <h2>Best Selling Products</h2>
+      <p>Speakers of many variations</p>
     </div>
-  
+
+    <div className='products-container'>
+      {productsData.map(
+        (products) => <Product key={products._id} product={products} />
+      )}
+    </div>
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+
 )
 
 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
-  const productsData = await client.fetch(query) 
+  const productsData = await client.fetch(query)
 
 
   const bannerQuery = '*[_type == "banner"]';
-  const bannerData = await client.fetch(bannerQuery) 
+  const bannerData = await client.fetch(bannerQuery)
 
   return {
     props: {
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,10 +1,13 @@
-import React from 'react'
-import {AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from 'react-icons/ai'
-import { client ,urlFor } from '../../lib/client'
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
+import { client, urlFor } from '../../lib/client'
 
-const ProductDetails = ({product, products}) => {
-  const {image, name, details, price} = product;
+import Product from '../../components/Product'
 
+const ProductDetails = ({ product, products }) => {
+  const { image, name, details, price } = product;
+
+  const [first, setfirst] = useState(second)
   return (
     <div>
       <div className='product-detail-container'>
@@ -23,38 +26,50 @@ const ProductDetails = ({product, products}) => {
           </div> */}
         </div>
         <div className='product-detail-desc'>
-            <h1>{name}</h1>
-            <div className='reviews'>
-              <div>
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiOutlineStar/>
-              </div>
-              <p>(20)</p>
-              
+          <h1>{name}</h1>
+          <div className='reviews'>
+
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
             </div>
-            <h4>Details: </h4>
-            <p>{details}</p>
-            <p className='price'>${price}</p>
-            <div className='quantity'>
-              <h3>Quantity: </h3>
-              <p className='quantity-desc'>
-                <span className='minus' onClick=''><AiOutlineMinus/></span>
-                <span className='num' onClick=''>0</span>
-                <span className='plus' onClick=''><AiOutlinePlus/></span>
-              </p>
-            </div>
-            <div className='buttons'>
-              <button type='button' className='add-to-cart' onClick=''>Add to Cart</button>
-              <button type='button' className='buy-now' onClick=''>Buy now</button>
-            </div>
+            <p>(20)</p>
+
+
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className='price'>${price}</p>
+          <div className='quantity'>
+            <h3>Quantity: </h3>
+            <p className='quantity-desc'>
+              <span className='minus' onClick=''><AiOutlineMinus /></span>
+              <span className='num' onClick=''>0</span>
+              <span className='plus' onClick=''><AiOutlinePlus /></span>
+            </p>
+          </div>
+          <div className='buttons'>
+            <button type='button' className='add-to-cart' onClick=''>Add to Cart</button>
+            <button type='button' className='buy-now' onClick=''>Buy now</button>
+          </div>
         </div>
       </div>
+
       <div className='maylike-products-wrapper'>
-            <h2>You may also like</h2>
-            
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product
+                key={item.id}
+                product={item}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
@@ -72,7 +87,7 @@ export const getStaticPaths = async () => {
 
   const paths = products.map((product) => ({
     params: {
-      slug: product.slug.current 
+      slug: product.slug.current
     }
   }));
 
@@ -83,13 +98,13 @@ export const getStaticPaths = async () => {
 }
 
 
-export const getStaticProps = async ({params: {slug}}) => {
+export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
   const productsQuery = '*[_type == "product"]'
 
 
-  const product = await client.fetch(query) 
-  const products = await client.fetch(productsQuery) 
+  const product = await client.fetch(query)
+  const products = await client.fetch(productsQuery)
 
 
   return {
